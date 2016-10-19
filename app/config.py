47e287620ff8e2d1e770829fcbd71773e133c3d8
Pierre Guillemot <pierre.guillemot@icflix.com>
2016-10-19 08:21:56 +0400

    Added APP_NAME, ENVIRONMENT and APP_TOKEN in config.py

--- app/config.py
+++ app/config.py
@@ -12,3 +12,9 @@ THREADS_PER_PAGE = 2
 SECRET_KEY = os.environ.get('SECRET_KEY', '9oup6z5mdbw)8(f5$9ob@m&xha*(5ulqot&x*y1n$1^^9qo#d-')
 
 NEW_RELIC_CONFIG_FILE = 'newrelic.ini newrelic-admin run-program command options'
+
+APP_NAME = os.environ.get('APP_NAME', 'flask-boilerplate')
+
+ENVIRONMENT = os.environ.get('ENVIRONMENT', 'staging')
+
+APP_TOKEN = os.environ.get('APP_TOKEN', '4b6f204b-04e8-489a-9aec-7d204e4cec34')
