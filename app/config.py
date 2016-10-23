4478a25c0dcf7f372cf91e66019595e43ca8ce86
Pierre Guillemot <pierre.guillemot@icflix.com>
2016-10-23 14:20:44 +0400

    #31 : Disabled flask-restful extra message for 404 responses

--- app/config.py
+++ app/config.py
@@ -5,6 +5,8 @@ DEBUG = os.environ.get('DEBUG', True)
 
 THREADS_PER_PAGE = 2
 
+ERROR_404_HELP = False
+
 SECRET_KEY = os.environ.get('SECRET_KEY', '9oup6z5mdbw)8(f5$9ob@m&xha*(5ulqot&x*y1n$1^^9qo#d-')
 
 NEW_RELIC_CONFIG_FILE = 'newrelic.ini newrelic-admin run-program command options'
