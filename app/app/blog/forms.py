49d4ac92b1da36e0620dce4adca0e51f2b41209d
Pierre Guillemot <pierre.guillemot@icflix.com>
2016-10-30 11:48:57 +0400

    #31 : Restructured the app

--- app/app/blog/forms.py
+++
@@ -1,6 +0,0 @@
-from wtforms import Form, StringField, validators
-
-
-class CreateOrUpdateBlogForm(Form):
-    title = StringField('title', [validators.required()])
-    content = StringField('content', [validators.required()])
--- src/manage.py
+++ src/manage.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 from app import app
 from flask_script import Manager, Server
-from flask_migrate import MigrateCommand
 
 manager = Manager(app)
 manager.add_command('runserver', Server())
-manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
